13fa7caf493f135019cd80ba99c13addcfedc658
panchen <panchen@tigerbrokers.com>
2021-10-12 21:22:43 +0800

    prepared app.

---
+++ app/src/main/java/me/bytebeats/compose/router/app/Constants.kt
@@ -0,0 +1,9 @@
+package me.bytebeats.compose.router.app
+
+/**
+ * Created by bytebeats on 2021/10/12 : 21:19
+ * E-mail: happychinapc@gmail.com
+ * Quote: Peasant. Educated. Worker
+ */
+
+internal const val TAG = "compose-router"
---
+++ app/src/main/java/me/bytebeats/compose/router/app/entity/Major.kt
@@ -0,0 +1,10 @@
+package me.bytebeats.compose.router.app.entity
+
+/**
+ * Created by bytebeats on 2021/10/12 : 21:06
+ * E-mail: happychinapc@gmail.com
+ * Quote: Peasant. Educated. Worker
+ */
+data class Major(val id: String) {
+    val name: String = "Major #$id"
+}
